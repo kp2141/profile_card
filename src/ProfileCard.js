ef91a8afad818c21b379fe762387e594a125b958
kp2141 <kp2141krp@gmail.com>
2023-02-02 18:01:14 -0500

    added more css to card and pages

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -5,22 +5,23 @@ const ProfileCard = (props) => {
   console.log(props);
 
   return (
-    <div className="App col-sm-4">
+    <div className="App col-sm-3">
       <Container >
         <Col >
           <Card>
+          <Card.Img variant="top" src={props.Img}  className="image-size" />
             <Card.Body>
               <Card.Title>{props.title}</Card.Title>
               <Card.Text>
                 <h2>
                     {props.handle}
                 </h2>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
                 vitae molestie magna. Vivamus sed molestie enim, eu convallis
                 mauris. Aliquam pharetra velit ac enim maximus, a commodo augue
-                hendrerit. Phasellus at aliquam est
+                hendrerit. Phasellus at aliquam est */}
               </Card.Text>
-              <Button variant="primary">Read More</Button>
+              {/* <Button variant="primary">Read More</Button> */}
             </Card.Body>
           </Card>
         </Col>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import ProfileCard from './ProfileCard';
 import { Container, Card, Col, Button,Row } from "react-bootstrap";
 import './index.css';
+import ImagAlexa from './images/alexa.png';
+import ImagCortana from './images/cortana.png';
+import ImagSiri from './images/siri.png';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,18 +15,23 @@ function App(){
   const columnsPerRow = 4
   return (
     
-    <div>
-      <div class="title">
+    <div className="text-center">
+      <div className="title alert alert-primary">
         <h1>
           Personal Digital Assisants
         </h1>
       </div>
-     
-      <div className="row">
-        <ProfileCard title="Alexa" handle="@alexa99"/>
-        <ProfileCard title="Cortana" handle="@cortana32"/>
-        <ProfileCard title="Siri" handle="@siri96"/>
+      <div className="container-flex">
+      <div className="row justify-content-center">
+        
+        <ProfileCard title="Alexa" handle="@alexa99" Img = {ImagAlexa}/>
+        <ProfileCard title="Cortana" handle="@cortana32" Img = {ImagCortana}/>
+        <ProfileCard title="Siri" handle="@siri96" Img = {ImagSiri}/>
+      
+        
       </div>
+      </div>
+    
       </div> 
   );
 }
